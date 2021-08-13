cef022b5f6b545872c25a1b11b57491f1ea28305
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-08-12 21:08:03 -0400

    added new files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,41 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jan', 'Doe', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ralph', 'Marvin', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Donnie', 'Donohue', 5, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Earl', 'Row', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Irvine', 'Springs', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ora', 'Rita',null ,null );
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Sales');
+INSERT INTO department (department_name)
+VALUES ('Accounting');
+INSERT INTO department (department_name)
+VALUES ('Reception');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
+INSERT INTO department (department_name)
+VALUES ('Intern');
+
+VALUES ('CEO', 850000, null);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 90000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Payroll', 60000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Resource Officer', 23000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesman', 20000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Secretary', 23000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Intern', 9000,null );
+INSERT INTO role (title, salary, department_id)
